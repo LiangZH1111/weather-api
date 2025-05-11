7d475486a2df63fe47bfd8a1715a4ff8209551b3
lzh13 <zhliang1111notes@gmail.com>
2025-05-11 22:08:15 +0800

    Add URL endpoint for yearly and all data at a station

--- main.py
+++ main.py
@@ -6,20 +6,37 @@ app = Flask(__name__)
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID","STANAME                                 "]]
 
+# Home page
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
 
+# For data on a date at a station
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
+def on_date(station, date):
     filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
     df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
     temperature = df.loc[df["    DATE"]=="1860-01-05"]["   TG"].squeeze() / 10
-    # temperature = 23
+
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
+# For all data at a station
+@app.route("/api/v1/<station>")
+def at_station(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    return result
+
+# For a year of data at a station
+@app.route("/api/v1/yearly/<station>/<year>")
+def in_year(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
 
 if __name__ == "__main__":
     app.run(debug=True)
